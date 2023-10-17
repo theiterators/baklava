48fb6ccd8fce5bb280d609b856001a9ccf3fa094
Krzysztof Palcowski <kpalcowski@iteratorshq.com>
2023-10-17 16:21:09 +0200

    Enums in query params

--- core/src/main/scala/pl/iterators/baklava/core/model/RouteParameterRepresentation.scala
+++ core/src/main/scala/pl/iterators/baklava/core/model/RouteParameterRepresentation.scala
@@ -6,10 +6,13 @@ case class RouteParameterRepresentation[T](
     name: String,
     required: Boolean,
     sampleValue: T,
-    marshaller: T => String
+    marshaller: T => String,
+    enumValues: Option[Seq[T]] = None
 )(implicit typeTag: TypeTag[T]) {
 
   lazy val marshall: String = marshaller(sampleValue)
+
+  lazy val enums: Option[Seq[String]] = enumValues.map(values => values.map(marshaller))
 
   lazy val scalaType: String = typeTag.tpe.toString
 }
